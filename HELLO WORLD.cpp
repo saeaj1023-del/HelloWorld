1b45f96eab1e11f9e153695522823b3a77cb9d99
saeaj1023-del <saeaj1023@gmail.com>
2025-09-15 17:03:25 +0900

    Initial commit

---
+++ HELLO WORLD.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	//data type(자료형), 정수 자료형
+	//변수이름은 잘짓기
+	//[][][1][1] 
+	//변수값은 마지막만 남음 
+	int gold = 11; // 메모리에 공간잡고 넣어주기
+	std::cout << "Hello World" << std::endl;
+	std::cout << gold << std::endl;
+	return 0;
+}
