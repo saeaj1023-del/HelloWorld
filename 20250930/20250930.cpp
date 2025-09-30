1dac5cfa24e3c12b495721f4863f1d6d4434cae6
saeaj1023-del <saeaj1023@gmail.com>
2025-09-30 17:49:08 +0900

    2

--- 20250930/20250930.cpp
+++ 20250930/20250930.cpp
@@ -2,23 +2,102 @@
 
 using namespace std;
 
+int Map[10][10] = {
+
+
+};
+
+class Engine
+{
+public:
+	World MyWorld;
+
+	void Run()
+	{
+		while (true)
+		{
+			Input();
+			Tick();
+			Render();
+		}
+
+	}
+	void Input()
+	{
+
+	}
+
+	void Tick()
+	{
+
+	}
+
+	void Render()
+	{
+
+	}
+};
+
+class World
+{
+public:
+	Player MyPlayer;
+	Monster MyMonster;
+	Wall MyWall[100];
+	Floor MyFloor[100];
+	Goal MyGoal;
+
+	void GameOver()
+	{
+
+	}
+};
+
+class Wall
+{
+public:
+
+};
+
+class Floor
+{
+public:
+
+};
+
+class Player
+{
+public:
+	void Move()
+	{
+
+	}
+};
+
+class Monster
+{
+public:
+	void Move()
+	{
+
+	}
+
+};
+
+class Goal
+{
+public:
+
+};
 
 int main()
 {
-	/*
-	int N;
-	using long long X;
-
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> N;
-	}
-	*/
-
-
+	//init()
+	Engine* MyEngine = new Engine();
+	//(*MyEngine).Run();
+	MyEngine->Run();
+	delete MyEngine;
+	MyEngine = nullptr;
 
 	return 0;
 }
-
-
